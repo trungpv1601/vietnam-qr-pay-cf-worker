a567859e91a9381516b35a725000522189b88c91
trungpv <trungpv1601@gmail.com>
2025-04-17 08:29:29 +0700

    🎯

--- src/index.js
+++ src/index.js
@@ -1,15 +1,50 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
+import { QRPay, BanksObject } from 'vietnam-qr-pay';
+const qr = require("qr-image");
 
-export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
-};
+const app = new Hono();
+
+// MBBank
+const BANK_NUMBER = '2700';
+// https://github.com/xuannghia/vietnam-qr-pay/blob/b48ad59882acc4cb10aa548636f05f811d30533c/src/constants/bank-key.ts
+const BANK_BIN = BanksObject.mbbank.bin;
+
+app.use(cors({
+    origin: '*',
+}));
+
+app.get('/', async (c) => {
+    try {
+        const amount = c.req.query('amount', '100000');
+        const purpose = c.req.query('purpose', 'Ung Ho NCHCCCL');
+
+        if (!amount || !purpose) {
+            return c.json({
+                success: false,
+                message: 'Please provide both amount and purpose',
+            }, 400);
+        }
+
+        const qrPay = QRPay.initVietQR({
+            bankBin: BANK_BIN,
+            bankNumber: BANK_NUMBER,
+            amount: amount,
+            purpose: purpose,
+        });
+        const content = qrPay.build();
+
+        c.header('Content-Type', 'image/png');
+
+        const qr_png = qr.imageSync(content);
+
+        return c.body(qr_png);
+    } catch (error) {
+        return c.json({
+            success: false,
+            message: error?.message || 'Something went wrong! Please try again.'
+        });
+    }
+});
+
+export default app;
